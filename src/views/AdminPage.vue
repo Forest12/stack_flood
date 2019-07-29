7008c09b24400bf6bf35e074fa6b7b2b2cceb6b7
mycamp <micro_dk@naver.com>
2019-07-29 09:43:18 +0900

    19.07.29|1 commit

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,8 +1,70 @@
 <template>
   <div>
-    관리자페이지입니다.
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 class="pink lighten-1">
+          <div class="admin-header">
+            Admin-Page
+          </div>
+        </v-flex>
+        <v-flex md3 xs6 class="indigo lighten-3">
+          <div class="list-title">유저 목록</div>
+        </v-flex>
+        <v-flex md3 xs6 class="indigo lighten-3">
+          <div class="list-title">권한</div>
+        </v-flex>
+        <v-flex md6 class="indigo lighten-3">
+           <div class="list-title">게시글 수 총 : nn 개</div>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex md3 xs6 class="info lighten-3">
+          <div class="gogo">
+            <input type="radio" id="user-1" name="username" value="유저1" v-model="username">
+            <label for="user-1">유저1</label>
+            <input type="radio" id="user-2" name="username" value="유저2" v-model="username">
+            <label for="user-2">유저2</label>
+            <input type="radio" id="user-3" name="username" value="유저3" v-model="username">
+            <label for="user-3">유저3</label>
 
-    {{$store.state.user.email}}
+          </div>
+        </v-flex>
+        <v-flex md3 xs6 class="info lighten-3">
+          <input class="rdo" type="radio" id="auth-manager" name="auth" value="manager" v-model="auth">
+          <label class="lbl" for="auth-manager">관리자</label>
+          <input class="rdo" type="radio" id="auth-team" name="auth" value="team" v-model="auth">
+          <label class="lbl" for="auth-team">팀원</label>
+          <input class="rdo" type="radio" id="auth-visitor" name="auth" value="visitor" v-model="auth">
+          <label class="lbl" for="auth-visitor">방문자</label>
+          
+        </v-flex>
+        <v-flex md6 class="info lighten-3">
+
+          <div class="zz">
+            <div class="posts-count">nn개</div>
+            <div class="posts-subject">빅데이터</div>
+          </div>
+          <div class="zz">
+            <div class="posts-count">nn개</div>
+            <div class="posts-subject">빅데이터</div>
+          </div>
+          <div class="zz">
+            <div class="posts-count">nn개</div>
+            <div class="posts-subject">빅데이터</div>
+          </div>
+          <div class="zz">
+            <div class="posts-count">nn개</div>
+            <div class="posts-subject">빅데이터</div>
+          </div>
+
+
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+
+
+   
 
     <div :class="{notAdmin : !isAdmin}">
       <div class="user-list">
@@ -10,12 +72,12 @@
           <div>유저 목록</div>
         </div>
         <div class="user-list-content">
-          <input type="radio" id="user-1" name="username" value="유저1" v-model="username">
+          <!-- <input type="radio" id="user-1" name="username" value="유저1" v-model="username">
           <label for="user-1">유저1</label>
           <input type="radio" id="user-2" name="username" value="유저2" v-model="username">
           <label for="user-2">유저2</label>
           <input type="radio" id="user-3" name="username" value="유저3" v-model="username">
-          <label for="user-3">유저3</label>
+          <label for="user-3">유저3</label> -->
           {{ username }}
 
         </div>
@@ -26,12 +88,12 @@
           <div>권한 변경</div>
         </div>
         <div class="authority-list-content">
-          <input class="rdo" type="radio" id="auth-manager" name="auth" value="manager" v-model="auth">
+          <!-- <input class="rdo" type="radio" id="auth-manager" name="auth" value="manager" v-model="auth">
           <label class="lbl" for="auth-manager">관리자</label>
           <input class="rdo" type="radio" id="auth-team" name="auth" value="team" v-model="auth">
           <label class="lbl" for="auth-team">팀원</label>
           <input class="rdo" type="radio" id="auth-visitor" name="auth" value="visitor" v-model="auth">
-          <label class="lbl" for="auth-visitor">방문자</label>
+          <label class="lbl" for="auth-visitor">방문자</label> -->
           
           {{ auth }}
         </div>
@@ -61,6 +123,7 @@ export default {
       isAdmin : true,
       username : '',
       auth: '',
+      posts:[],
     }
   },
   created() {
@@ -70,6 +133,11 @@ export default {
       FirebaseService.getPosts('member').then(res => 
         console.log(res[0].user_authority))
       // this.isAdmin = false
+
+      FirebaseService.getPosts('Bigdata').then(res => console.log(res[0]));
+
+
+
     } else {
 
     }
@@ -106,7 +174,37 @@ export default {
   background:red;
 }
 
+.gogo {
+  height:150px;
+}
 
+.zz {
+  display: inline-block;
+  width: 50%;
+  height:50%;
+}
 
+.admin-header {
+  height:80px;
+  font-size:4.2rem;
+  text-align:center;
+  border: 1px solid black;
+  box-sizing:border-box;
+  
+}
 
+.list-title {
+  height:50px;
+  padding:15px;
+  border: 1px solid black;
+  box-sizing:border-box;
+}
+
+.posts-count {
+  font-size:20px;
+  position:relative;
+  top:30%;
+  left:35%;
+  
+}
 </style>
