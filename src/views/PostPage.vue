020f510f5ae0be98c158846608e7c9d3591ba591
mycamp <micro_dk@naver.com>
2019-08-07 12:14:05 +0900

    19.08.07|CSS작업

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="co">
     <!-- Img Banner -->
-
+    
 
     <!-- Post list -->
-    <v-container>
+    <!-- <v-container>
         <v-btn v-if="user_check" @click="newPost"  color="#FFA500" dark ><v-icon dark left small>fas fa-plus</v-icon>New</v-btn>
         <v-layout>
         <v-flex xs12>
@@ -13,9 +13,97 @@
         </v-flex>
       </v-layout>
       
+    </v-container> -->
+    <!-- <v-btn v-if="user_check" @click="newPost"  color="#FFA500" dark ><v-icon dark left small>fas fa-plus</v-icon>New</v-btn> -->
+    <v-container class="post-container">
+      <div class="post-box">
+        <div class="post-left">
+          <h2 class="white--text">{{item}}</h2>
+        </div>
+
+        <div class="post-right">
+          <v-layout>
+          <v-flex xs12>
+              <PostList :item="item" :limits="10" :load-more="true"></PostList>
+          </v-flex>
+        </v-layout>
+        </div>
+      
+        <div class="post-postit"></div>
+      </div>
+
     </v-container>
   </div>
 </template>
+
+<style scoped>
+.co{
+  display:flex;
+  justify-content: center;
+}
+.post-container {
+  margin:20px;
+  width:100%;
+  overflow:auto;
+  /* border:2px solid orange; */
+}
+
+.post-box{
+  display:flex;
+  height:100%;
+  width:100%;
+}
+
+.post-left {
+  width:25%;
+  float:left;
+  background:#2E303E;
+}
+
+.post-postit{
+  background: black;
+  width:150px;
+}
+
+.post-right {
+  width:70%;
+  float:left;
+}
+
+@media screen and (max-width:1000px) and (min-width:700px){
+  .post-postit{
+    display:none;
+    width:0px;
+  }
+  .post-right{
+    display: contents;
+  }
+}
+
+@media screen and (max-width: 700px){
+  .post-box{
+    height:100%;
+    width:100%;
+    display:block;
+  }
+  .post-left{
+    display:block;
+    height:150px;
+    width:100%;
+    background:#2E303E;
+  }
+  .post-postit{
+    width:0px;
+    display:none;
+  }
+  .post-right {
+    width:100%;
+    float:left;
+}
+}
+
+
+</style>
 
 <script>
 import FirebaseService from '@/services/FirebaseService'
